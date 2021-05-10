8a1c1d3ba3a1585a63d9c1e1598c306e3daef903
Artur <info@aigroupusa.com>
2021-05-10 17:53:36 -0400

    add number table

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,3 +1,29 @@
 class Number < ActiveRecord::Base
+    
+
+    def fill_table
+        @draws = Draw.all
+        @draws.each do |draw|
+            arr = draw.winning_numbers.split(" ").first(5)
+           @number = Number.new(add_data(arr))
+        end
+    end
+
+
+
+    
+    def add_data (draw)
+        hash = Hash.new
+        (1..69).each do |num|
+            b = "num_#{num}"
+            if draw.include?(num)
+            hash[b]=1
+            else
+            hash[b] = 0
+            end
+        end
+        hash
+    end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -78,10 +78,37 @@ URL = "https://data.ny.gov/resource/d6yy-54nr.json"
         response = Net::HTTP.get_response(uri)
         results = JSON.parse(response.body)
         results.each do |d| 
-         draw = Draw.new(d)
-         draw.save
+         @draw = Draw.new(d)
+         @draw.save
+        end
+
+        @draws = Draw.all
+        @draws.each do |draw|
+            arr = draw.winning_numbers.split(" ").first(5)
+            hash = Hash.new
+            (1..69).each do |num|
+                b = "num_#{num}"
+                if arr.include?(num)
+                hash[b]=1
+                else
+                hash[b] = 0
+                end
+            end
+           @number = Number.new(hash)
+           @number.save
         end
 
         
 
-  
+        # def add_data (draw)
+        #         hash = Hash.new
+        #         (1..69).each do |num|
+        #             b = "num_#{num}"
+        #             if draw.include?(num)
+        #             hash[b]=1
+        #             else
+        #             hash[b] = 0
+        #             end
+        #         end
+        #         hash
+        #     end
